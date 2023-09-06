8d915d1faf3184d27cd32851697efea9fb209287
Dmitry <melkdim@list.ru>
2023-09-06 22:24:09 +0500

    add

--- src/app/pages/community/create-community/create-community.component.ts
+++ src/app/pages/community/create-community/create-community.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SVG_ICONS } from '../../../common/injection-tokens/svg-icons.token';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-create-community',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AngularSvgIconModule],
   templateUrl: './create-community.component.html',
   styleUrls: ['./create-community.component.scss'],
 })
